eb2ced77148ccbe27b4c4f7c070de05fa7889b81
James Young <jpblicer@gmail.com>
2024-12-11 13:01:12 +0900

    parse input into separate arrays representing the corresponding rows

--- day_01/main.rb
+++ day_01/main.rb
@@ -1,6 +1,16 @@
 def main
-  input = File.open("input", "r")
-  puts input
+  rowA = []
+  rowB = []
+  input = File.open("input", "r") do |f|
+    f.each_line do |line|
+      numbers = line.split(' ').each_with_index do |number, index|
+        puts "next is #{number}with index of #{index}"
+        index == 0 ? rowA << number : rowB << number
+      end
+    end
+  end
+  puts rowA
+  puts rowB
 end
 
 main
