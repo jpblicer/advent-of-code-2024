bc2f051b932656fc7e8870784fa188439a9c204d
James Young <jpblicer@gmail.com>
2024-12-11 13:13:22 +0900

    sort rows

--- day_01/main.rb
+++ day_01/main.rb
@@ -1,16 +1,23 @@
-def main
+def order_rows(rowA, rowB)
+  sorted_rowA = rowA.map.sort
+  sorted_rowB = rowB.map.sort
+end
+
+def parse_file_rows
   rowA = []
   rowB = []
   input = File.open("input", "r") do |f|
     f.each_line do |line|
       numbers = line.split(' ').each_with_index do |number, index|
-        puts "next is #{number}with index of #{index}"
         index == 0 ? rowA << number : rowB << number
       end
     end
   end
-  puts rowA
-  puts rowB
+  order_rows(rowA, rowB)
+end
+
+def main
+  parse_file_rows
 end
 
 main
