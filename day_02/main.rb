81e26824424008a86e67e014dff04beb2b33ff28
James Young <jpblicer@gmail.com>
2024-12-12 13:43:52 +0900

    convert each line of file into array of integers

--- day_02/main.rb
+++ day_02/main.rb
@@ -1,9 +1,20 @@
-def main
-  input = File.foreach("input", "r") do |line|
-    puts line
+def line_to_array(line)
+  array_line = line.split
+  integer_array_line = array_line.map { |num| num.to_i }
+  p integer_array_line
+end
+
+def deserializer
+  input = File.foreach("input", "r") do |content|
+    lines = content.split("\n")
+    lines.each do |line|
+      line_to_array(line)
+    end
   end
 end
 
-
+def main
+  deserializer
+end
 
 main
