fdf8fce4cdb39fb5dfcf75444836b7acf714fe2b
James Young <jpblicer@gmail.com>
2024-12-13 19:10:36 +0900

    check problem is dampened

--- day_02/main.rb
+++ day_02/main.rb
@@ -8,11 +8,47 @@ def consistent_change_safe?(report)
   end
 end
 
-def distance_safe?(report)
+
+def safe_report_with_dampener?(levels)
+  # Check if the report is safe without removing any level
+  return true if is_safe_report?(levels)
+
+  # Try removing one level at a time and check if it becomes safe
+  (0...levels.size).each do |i|
+    new_levels = levels[0...i] + levels[(i+1)..-1]
+    return true if is_safe_report?(new_levels)
+  end
+
+  false  # If none of the levels can be removed to make the report safe
+end
+
+
+def second_check?(report)
   report.each_cons(2) do |first, second|
     distance = first - second
     if distance.abs > 3 || distance.abs == 0
       return false
+    end
+    true
+  end
+end
+
+def problem_dampened?(report)
+  report.each_cons(2) do |first, second|
+    distance = first - second
+    if distance.abs > 3 || distance.abs == 0
+      report.delete(report.find_index(first))
+      second_check?(report)
+    end
+    true
+  end
+end
+
+def distance_safe?(report)
+  report.each_cons(2) do |first, second|
+    distance = first - second
+    if distance.abs > 3 || distance.abs == 0
+      problem_dampened?(report)
     end
     true
   end
@@ -53,5 +89,5 @@ main
 
 
 # Test
-# test_reports =[[7,6,4,2,1],[1,2,7,8,9],[9,7,6,2,1],[1,3,2,4,5],[8,6,4,4,1],[1,3,6,7,9]]
-# safety_check(test_reports)
+test_reports =[[7,6,4,2,1],[1,2,7,8,9],[9,7,6,2,1],[1,3,2,4,5],[8,6,4,4,1],[1,3,6,7,9]]
+safety_check(test_reports)
